b96796174f97aac9b02effcf24c928785d392a88
ThreadsOfJoy <ThreadsOfJoy@ThreadsOfJoy-PC>
2019-10-10 17:19:29 +0530

    Succefully Loaded Static Resources

---
+++ onlineShopping/src/main/java/net/supdip/onlineShopping/controller/PageController.java
@@ -0,0 +1,39 @@
+package net.supdip.onlineShopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+// we will be creating a controller class that will handle various url mapping like /, /index, /home all will be mapped to the same
+// method handler. It will generate the data (greeting in our case) and along with it the logical view name.
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value = {"/", "/home", "/index" })
+	public ModelAndView index() {
+
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome to spring web mvc");
+		return mv;
+	}	
+	/*
+	 * @RequestMapping(value="/test") public ModelAndView
+	 * test(@RequestParam(value="greeting", required = false)String greeting) {
+	 * if(greeting == null) { 
+	 * greeting = "Hello there"; 
+	 * } 
+	 * ModelAndView mv = new ModelAndView("page"); 
+	 * mv.addObject("greeting", greeting); 
+	 * return mv; }
+	 */
+	
+	/*
+	 * @RequestMapping(value="/test/{greeting}") public ModelAndView
+	 * test(@PathVariable("greeting")String greeting) { if(greeting == null) {
+	 * greeting = "Hello there"; } ModelAndView mv = new ModelAndView("page");
+	 * mv.addObject("greeting", greeting); return mv; }
+	 */
+}
